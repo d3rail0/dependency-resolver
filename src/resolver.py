5880217da26ef9bbe1928f821af1517dc0bde46b
Eldin <eldinhelja@live.com>
2022-01-19 15:41:21 +0100

    Mouse pos interpolation, file dialog browser, messagbox and more

--- src/resolver.py
+++ src/resolver.py
@@ -1,4 +1,17 @@
-import os, time, pygame
+import sys, os, time
+import src.utils.util as util 
+
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
+
+# GUI
+import tkinter
+from tkinter import messagebox
+from tkinter import filedialog as fd
+
+# Render engine
+import pygame
+
+# State
 from src.states.menu import Menu
 
 class Resolver():
@@ -6,28 +19,51 @@ class Resolver():
     def __init__(self) -> None:
         pygame.init()
 
-        self.VIEW_W  , self.VIEW_H   = 800, 600
-        self.SCREEN_W, self.SCREEN_H = 800, 600
+        # Don't show tkinter window
+        tkinter.Tk().withdraw()
+        
+        self.__init_assets()
+
+        icon = pygame.image.load(os.path.join(self.assets_dir, "icon.ico"))
+        pygame.display.set_icon(icon)
+
+        self.VIEW_W  , self.VIEW_H   = 1024, 860
+        self.SCREEN_W, self.SCREEN_H = 1024, 860
 
         self.canvas          = pygame.Surface((self.VIEW_W, self.VIEW_H))
         self.display_surface = pygame.display.set_mode((self.SCREEN_W, self.SCREEN_H), pygame.RESIZABLE)
 
         self.is_running  = True
         self.state_stack = []
-        self.actions = {"left": False, "right": False, "up" : False, "down" : False, "action1" : False, "action2" : False}
-        self.dt, self.prev_time = 0, 0
-
-        self.mouse_x, self.mouse_y = 0, 0
-
-        self.__init_assets()
+        self.fps_list    = []
+        self.actions     = {"left": False, "right": False, "up" : False, "down" : False, "m_down" : False, "m_up" : False, "escape": True}
+        
+        self.dt     , self.prev_time = 0, 0
+        self.mouse_x, self.mouse_y   = 0, 0
+        
         self.__init_states()
+        self.__update_interpolators()
 
     def main_loop(self) -> None:
         while self.is_running:
             self.__update_dt()
             self.__capture_events()
             self.__update()
+
+            # Exit cleanly
+            if len(self.state_stack)==0:
+                self.is_running = False
+                break
+
             self.__render()
+
+    def __update_interpolators(self):
+        self.interp_x = util.make_interpolator([0, self.display_surface.get_size()[0]], [0, self.VIEW_W])
+        self.interp_y = util.make_interpolator([0, self.display_surface.get_size()[1]], [0, self.VIEW_H])
+
+    def __fix_mouse_pos(self):        
+        self.mouse_x = int(self.interp_x(self.mouse_x))
+        self.mouse_y = int(self.interp_y(self.mouse_y))
 
     def __capture_events(self) -> None:
         for event in pygame.event.get():
@@ -35,14 +71,41 @@ class Resolver():
                 self.is_running = False
             elif event.type == pygame.MOUSEMOTION:
                 self.mouse_x, self.mouse_y = event.pos
+            elif event.type == pygame.MOUSEBUTTONDOWN:                
+                self.mouse_x, self.mouse_y = event.pos
+                self.actions['m_down'] = True
             elif event.type == pygame.MOUSEBUTTONUP:
                 self.mouse_x, self.mouse_y = event.pos
-                self.actions['action1'] = True
-                           
+                self.actions['m_up'] = True
+            elif event.type == pygame.WINDOWRESIZED:
+                self.__update_interpolators()
+            
+            # Handle key presses
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    self.actions['escape'] = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_ESCAPE:
+                    self.actions['escape'] = False
+                
+            
+            
+            self.__fix_mouse_pos()
+
     def __update_dt(self) -> None:
         now            = time.time()
         self.dt        = now - self.prev_time
         self.prev_time = now
+        self.get_fps()
+
+    def get_fps(self):
+        fps = 0
+        if self.dt: fps = 1/self.dt
+        if len(self.fps_list) == 50:
+                self.fps_list.pop(0)
+        self.fps_list.append(fps)
+        avg_fps = sum(self.fps_list) / len(self.fps_list)
+        pygame.display.set_caption('Dependency resolver - FPS: ' + str(round(avg_fps,2)))
 
     def __update(self) -> None:
         self.state_stack[-1].update(self.dt, self.actions)
@@ -53,9 +116,9 @@ class Resolver():
         pygame.display.flip()
     
     def __init_assets(self) -> None:
-        self.assets_dir = os.path.join("./assets")
-        self.font_dir   = os.path.join(self.assets_dir, "font")
-        self.font       = pygame.font.SysFont("consolas", 14)
+        self.assets_dir = os.path.join("assets")
+        self.font_dir   = os.path.join(self.assets_dir, "fonts")
+        self.font       = pygame.font.Font(os.path.join(self.font_dir, "NunitoRegular-vmABZ.ttf"), 20)
 
     def __init_states(self) -> None:
         self.menu_state = Menu(self)
@@ -70,3 +133,20 @@ class Resolver():
     def reset_keys(self) -> None:
         for action in self.actions:
             self.actions[action] = False
+
+    def focus(self) -> None:
+        """ Focuses pygame window.
+        """
+        util.raise_window(pygame.display.get_caption()[0])
+
+    def show_error(self, title, err_txt) -> None:
+        messagebox.showerror(title, err_txt)
+        self.focus()
+
+    def find_directory(self) -> str:
+        """ Opens a file dialog browser.
+        Returns: Path to selected directory.
+        """
+        target_dir = fd.askdirectory()
+        self.focus()
+        return target_dir
