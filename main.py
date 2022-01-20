b028f61ee8de4c5783939b73116c22c33495fa1b
Eldin <eldinhelja@live.com>
2022-01-20 23:58:51 +0100

    Main init

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from global_logger import Log
 import os
+from global_logger import Log
+from src.resolver import *
 
 glob_log = Log.get_logger(name="main", logs_dir=os.path.abspath("./log"))
 
 def main():
-    pass
+    resolver_app = Resolver()
+    resolver_app.main_loop()
 
 if __name__ == "__main__":
     try:
