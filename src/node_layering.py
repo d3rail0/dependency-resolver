c0031bbf9de0df78ef949335603efcf28b4f7d9b
Eldin <eldinhelja@live.com>
2022-01-21 19:23:45 +0100

    Preparing new data strucutre for proper layering

--- src/node_layering.py
+++ src/node_layering.py
@@ -1,23 +1,55 @@
 from src.ds.graph import *
 
-def longest_path_layering(graph: DirectedGraph, topological_order: list[int]) -> list[int]:
-    """ Computes min-height layers for vertices sorted in topological order
-    and.
-    Returns: List of layer levels where item at index i represents
-    a layer level of a vertex i. 
-    """
+class Node: 
 
-    layers = [1]*len(topological_order)
+    def __init__(self, node_id, is_dummy:bool = False) -> None:
+        self.node_id = node_id
+        self.is_dummy = is_dummy
 
-    for node in topological_order:
+class Layer:
 
-        for node_out in graph.get_neighbors_out(node):
+    def __init__(self, layer_level) -> None:
+        self.__layer_level = layer_level
+        self.__nodes: list[Node] = []
 
-            min_length = 1
+    def add_node(self, node_id):
+        self.__nodes.append(
+            Node(node_id, False)
+        )
+    
 
-            layers[node_out] = max(
-                layers[node_out],
-                layers[node] + min_length
-            ) 
+class Layering:
 
-    return layers
+    @property
+    def layers(self) -> list[int]:
+        """Returns: List of layer levels where item at index i represents
+        a layer level of a vertex i. 
+        """
+        return self.__layers
+
+    def __init__(self, graph: DirectedGraph, topological_order: list[int]) -> None:
+          
+        self.graph = graph
+        self.topological_order = topological_order
+        self.__layers: list[int] = []
+
+    def compute_layers(self):
+        """ Computes min-height layers for vertices sorted in topological order
+        and.
+        """
+        self.__layers = [1]*(len(self.graph)+1)
+
+        for node in self.topological_order:
+
+            for node_out in self.graph.get_neighbors_out(node):
+
+                min_length = 1
+
+                self.__layers[node_out] = max(
+                    self.__layers[node_out],
+                    self.__layers[node] + min_length
+                ) 
+        
+
+    def proper_layering(graph: DirectedGraph, layerings: list[int]) -> list[int]:
+        pass
